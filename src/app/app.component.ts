96ae34ed8ff79e2ee14f77fe2d41933e9cbdd573
benjith <mpbentjith@gmail.com>
2019-01-08 15:05:46 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import * as jsPDF from 'jspdf'
+import * as FileSaver from 'file-saver';
+import * as XLSX from 'xlsx';
+import { ExcelService } from './excel_service';
+import { DomEventsPlugin } from '@angular/platform-browser/src/dom/events/dom_events';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'report';
+  constructor(private excelService:ExcelService){
+
+  }
+  data: any = [{
+    eid: 'e101',
+    ename: 'ravi',
+    esal: 1000
+  },
+  {
+    eid: 'e102',
+    ename: 'ram',
+    esal: 2000
+  },
+  {
+    eid: 'e103',
+    ename: 'rajesh',
+    esal: 3000
+  }];
+  _pdfExport(){
+    var doc = new jsPDF();
+        var element = '<button>html elemet</button>'
+        doc.fromHTML(element,10,10)
+        doc.autoPrint() //if auto print needed
+        doc.save('Test.pdf');   // Save the PDF
+  }
+  _exelExport(){
+  
+    this.excelService.exportAsExcelFile(this.data, 'sample');
+    
+  }
+
+
 }
